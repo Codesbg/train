39b91e216c329ae2679bbda6bde17165b85e50e6
SHITIJ BHARTI GOSWAMI <125106033+Codesbg@users.noreply.github.com>
2024-11-13 08:19:43 +0530

    Add files via upload

---
+++ gec.js
@@ -0,0 +1,12 @@
+var q= 2344;
+function hello () {
+    console.log("sbg")
+}
+console.log(q);
+hello()
+    
+const w=890;
+function print (){
+console.log(w);
+}
+print()
---
+++ index.js
@@ -0,0 +1,13 @@
+var a=23;
+
+if (a>=18) {
+    console.log("you can vote");
+    
+}
+else if (a<18){
+    console.log("you can not vote");
+    
+}
+else{
+    console.log("123");
+}
